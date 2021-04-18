85c6808878b346919fefff33c58b6733ec8e5ccc
ACCOUNT MAIL SERVICES <jasimarifali@gmail.com>
2021-04-18 16:09:50 +0500

    port added

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req,res)=>{
     res.render('index')
 })
-
-app.listen(8000, ()=>{
-    console.log("app is listening at port 8000")
+const PORT = process.env.PORT || 8000
+app.listen(PORT, ()=>{
+    console.log(`app is listening at port ${PORT}`)
 })
